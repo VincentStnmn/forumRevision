0746b0c4af73ddf206d30f6f4a80c03f45e53063
STEINMANNV <STEINMANNV@lab.ecinf.ch>
2018-08-30 13:57:20 +0200

    1er commit

---
+++ public/php/index.php
@@ -0,0 +1,22 @@
+<!-----
+Title: recap
+Author: Vincent Steinmann
+Date: 30.08.2018
+Desc.: Site to test our knowledge
+------>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+    </head>
+    <body>
+        <form>
+            <fieldset>
+                <legend>Connection</legend>
+                <label>Identifiant</label>
+                <input type="text" name="id">
+            </fieldset>
+        </form>
+    </body>
+</html>
